c33c282530b81cbd4030834f5910faed5d27e01f
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-10-31 15:16:42 +0200

    Card click fade

--- Project Z-Bag/Assets/Scripts/Card.cs
+++ Project Z-Bag/Assets/Scripts/Card.cs
@@ -1,12 +1,55 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour , IPointerClickHandler
 {
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject cardVisual;
+    [SerializeField] private float fadeAmount = 0f;
+    [SerializeField] private float fadeSpeed = 1f;
+    private bool isFaded = false;
+    private Material originalMaterial;
+
+    private void Start()
     {
-        
+        originalMaterial = cardVisual.GetComponent<Renderer>().material;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isFaded)
+        {
+            // If the object is currently faded, fade it back to full opacity
+            Debug.Log("Fading back in");
+            StartCoroutine(FadeCard(originalMaterial.color.a, 1f));
+        }
+        else
+        {
+            // If the object is not faded, fade it to the target opacity (faded)
+            Debug.Log("The object has been clicked");
+            StartCoroutine(FadeCard(originalMaterial.color.a, fadeAmount));
+        }
+
+        // Toggle faded state so it only changes once per click
+        isFaded = !isFaded;
+    }
+
+    private IEnumerator FadeCard( float startAlpha, float targetAlpha)
+    {
+        float elapsedTime = 0f;
+        Color startColor = new(originalMaterial.color.r, originalMaterial.color.g, originalMaterial.color.b, startAlpha);
+        Color targetColor = new(startColor.r, startColor.g, startColor.b, targetAlpha);
+
+        while (elapsedTime < fadeSpeed)
+        {
+            // Interpolate the alpha value over time
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeSpeed);
+            originalMaterial.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Ensure the final colour is set
+        originalMaterial.color = targetColor;
     }
 }
