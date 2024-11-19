f7641824d0a052cf798b186553bc50fcf2d849f6
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-11-19 13:30:05 +0200

    Create item manager

---
+++ Project Z-Bag/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField] private GameObject itemPrefab;
+    [SerializeField] private CardManager cardManager;
+
+    private void Start()
+    {
+        cardManager.OnMatchFound.AddListener(DisplayObject);
+    }
+
+    private void DisplayObject()
+    {
+        GameObject itemObject = Instantiate(itemPrefab, itemPrefab.transform.position, itemPrefab.transform.rotation);
+
+
+    }
+}
